a80ec7b5fba51d932924661b66474d1664fffe4d
jaxnalia <jacksendall@gmail.com>
2024-08-28 23:11:12 +0100

    photos page removed

--- app/src/lib/Nav.svelte
+++ app/src/lib/Nav.svelte
@@ -1,7 +1,6 @@
 <div class="p-4 flex flex-row md:flex-col mt-0 gap-2 items-center md:items-start">
     <a href="/"><p class="text-2xl font-bold whitespace-nowrap">Jack Sendall</p></a>
     <a class="text-black" href="/">About</a>
-    <a class="text-black" href="/photos">Photos</a>
     <a class="text-black" href="https://portfolio.jack.sbs/">Portfolio</a>
 
 </div>
