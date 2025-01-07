343b2a3c91313c3e1bf74a4033712b501384f505
jaxnalia <jacksendall@gmail.com>
2025-01-07 04:02:44 +0000

    carousel

--- app/src/lib/LogoCarousel.svelte
+++ app/src/lib/LogoCarousel.svelte
@@ -56,7 +56,7 @@
         content: '';
         position: absolute;
         top: 0;
-        width: 15%;
+        width: 10%;
         height: 100%;
         z-index: 2;
         pointer-events: none;
