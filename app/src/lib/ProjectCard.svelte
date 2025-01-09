1391dbe7efc35d3e6df81796b0329e1fd4f7e676
jaxnalia <jacksendall@gmail.com>
2025-01-09 01:25:43 +0000

    external links

--- app/src/lib/ProjectCard.svelte
+++ app/src/lib/ProjectCard.svelte
@@ -17,7 +17,7 @@ export interface Project {
 
 export let project: Project;
 
-const externalLinkIcon = `<svg class="w-5 h-5 ml-2 hidden group-hover:inline text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"/></svg>`;
+const externalLinkIcon = `<svg class="w-5 h-5 ml-2 inline text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"/></svg>`;
 </script>
 
 <div class="relative">
--- app/src/lib/Timeline.svelte
+++ app/src/lib/Timeline.svelte
@@ -8,6 +8,9 @@
     };
   
     export let items: TimelineItem[] = [];
+
+    const externalLinkIcon = `<svg class="w-5 h-5 ml-2 inline text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"/></svg>`;
+
   </script>
   
   <div class="relative container mx-auto py-8 translate-y-7">
@@ -23,7 +26,8 @@
         </div>
         <div class="ml-8">
             {#if item.link}
-            <a href={item.url} class="text-xl absolute" >{item.link}</a>
+            <a href={item.url} class="text-xl absolute no-underline hover:underline" >{item.link}{@html externalLinkIcon}</a>
+            
             {/if}
             
             <h3 class="text-xl font-bold mt-7 mb-0">{item.title}</h3>
