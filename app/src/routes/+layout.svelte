a8813341bfbb90944a6c1fab2653036e9e7ee814
jaxnalia <jacksendall@gmail.com>
2025-01-12 17:30:45 +0000

    added vercel, aws

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 import "../app.css";
 import Nav from "$lib/Nav.svelte";
 </script>
-<div class="flex flex-col md:flex-row md:pt-16 md:pb-16 bg-white dark:bg-black">
+<div class="flex flex-col md:flex-row md:pt-16 md:pb-16 bg-black">
 
     <div class="" style=""><Nav /> </div>    
     
