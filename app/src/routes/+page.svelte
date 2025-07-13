580ded1b9c3635131446f93dbc17b45b0ff5c4b9
jaxnalia <jacksendall@gmail.com>
2025-07-13 02:31:39 +0100

    cc

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -48,10 +48,10 @@ const timelineItems = [
         <img src={avatar} alt="jack" class="w-96 mt-0 rounded-xl">
     </div>
     <div class="">
-        <h1 data-nosnippet class="mb-0 mt-8 font-bold">About me</h1>
-        <p class="text-xl">
+        <!-- <h1 data-nosnippet class="mb-0 mt-8 font-bold">About me</h1> -->
+        <!-- <p class="text-xl">
           Hi, 🤝 I'm a seasoned developer with a passion for solving problems and shipping fast.
-        </p>
+        </p> -->
         <!-- <p class="text-xl">
         <a href="/projects">Stuff I've made</a>
         </p> -->
@@ -60,7 +60,7 @@ const timelineItems = [
             <button
             type="submit"
             class="w-full bg-black group hover:bg-white border border-gray-400 py-3 px-6 rounded-lg">
-            <h3 class="m-0 text-white font-bold group-hover:text-black">See My Work</h3>
+            <h3 class="m-0 text-white font-bold group-hover:text-black">My Projects</h3>
           </button>
         </a>
         <a class="w-full" href="#contact">
@@ -84,8 +84,8 @@ const timelineItems = [
           Business enquiries: <a href="mailto:hello@jack.sbs">hello@jack.sbs</a>
         </p> -->
     </div>
-    <h1 data-nosnippet class="mb-8 mt-8 font-bold">Tools I use</h1>
-    <LogoCarousel />
+    <!-- <h1 data-nosnippet class="mb-8 mt-8 font-bold">Tools I use</h1>
+    <LogoCarousel /> -->
     <h1 data-nosnippet class="mb-0 mt-8 font-bold">History</h1>
     <Timeline items={timelineItems} />
     <h1 id="contact" class="mb-0 mt-8 font-bold">Let's Cook.</h1>
