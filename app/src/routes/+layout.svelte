436cf42ba22521b02700e6569d28580f45efda23
jaxnalia <jacksendall@gmail.com>
2025-10-06 22:44:04 +0100

    --

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -5,9 +5,6 @@ import Nav from "$lib/Nav.svelte";
 <div class="flex flex-col tablet:flex-row tablet:pt-16 tablet:pb-16 bg-black">
 
     <div class="" style=""><Nav /> </div>    
-    
-
-    
     <div class="flex flex-col flex-grow p-4">
         <slot></slot>
     </div>
